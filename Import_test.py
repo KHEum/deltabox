1b37c2a5aa12acd1edaf7c737cef5abf00fec828
KHEum <51514833+KHEum@users.noreply.github.com>
2020-03-07 21:26:19 +0900

    ver.01

---
+++ Import_test.py
@@ -0,0 +1,12 @@
+# import foobar as ft
+# ft.foo()
+# ft.bar()
+
+# import foobar
+# foobar.foo()
+# foobar.bar()
+
+from foobar import foo, bar
+
+foo()
+bar()
---
+++ test01.py
@@ -0,0 +1,33 @@
+# from random import *
+
+# # day = int((random() * 27))+1
+# day = randint(4, 28)
+# print("오프라인 스터디 모임은 날짜는 매월" + str(day) + "일로 선정되었습니다.")
+
+
+# jumin = "991010-1234567"
+# print("sex : " + jumin[7])
+
+# print("Apple is start %c " %"AB")
+# print("Apple is start %s " %"AB")
+# print("Apple is start %c%c " %("A","B"))
+
+# url = "http://naver.com"
+
+# my_str = url.replace["http//", ""]
+
+# my_str = my_str[]
+# # print(add1)
+# subway = ["조세호", "정형동", "유재석"]
+# print(subway)
+# print(subway.index("조세호"))
+# subway.append("하하")
+# print(subway)
+# subway.insert(0, "김말동")
+# print(subway)
+# print(subway.pop())
+# print(subway.pop(1))
+
+num_list = [4,5,7,34,2,]
+num_list.sort()
+print(num_list)
