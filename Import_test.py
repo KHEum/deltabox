bcc92af99815ad2f9b04bcb542a184e311f3db76
KHEum <51514833+KHEum@users.noreply.github.com>
2020-03-07 21:42:38 +0900

    ver.03

--- Import_test.py
+++ Import_test.py
@@ -6,7 +6,20 @@
 # foobar.foo()
 # foobar.bar()
 
-from foobar import foo, bar
+# from foobar import foo, bar
 
-foo()
-bar()
+# foo()
+# bar()
+
+def setdata(x, y):
+    z = x + y
+    return z
+
+
+input1 = 1
+input2 = 2
+outut = setdata(input1, input2)
+
+print(outut)
+
+
